cbf6ef9025ab92a1f7d165f81fadda84a8a61e76
Mark Lawrence <mark.lawrence@southernpoverty.onmicrosoft.com>
2017-03-13 08:38:57 -0500

    Implement web WOT configuration

---
+++ web/Controllers/ConfigurationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CRMC.DataAccess;
+using CRMC.Domain;
+
+namespace web.Controllers
+{
+    public class ConfigurationController : ApiController
+    {
+        private readonly DataContext _context = DataContext.Create();
+
+        public object Get()
+        {
+            var config = _context.AppSettings.FirstOrDefault();
+            return Ok(config);
+        }
+
+        public object Post([FromBody] AppConfig model)
+        {
+            var config = _context.AppSettings.Find(model.Id);
+
+            if (config == null) return NotFound();
+
+            config.HubName = model.HubName;
+            config.WebServerURL = model.WebServerURL;
+            config.Volume = model.Volume;
+            config.ScrollSpeed = model.ScrollSpeed;
+            config.AddNewItemSpeed = model.AddNewItemSpeed;
+            config.MinFontSize = model.MinFontSize;
+            config.MaxFontSize = model.MaxFontSize;
+            config.MinFontSizeVIP = model.MinFontSizeVIP;
+            config.MaxFontSizeVIP = model.MaxFontSizeVIP;
+            config.AudioFilePath = model.AudioFilePath;
+
+            _context.AppSettings.AddOrUpdate(model);
+            _context.SaveChanges();
+            return Ok(model);
+        }
+    }
+}
---
+++ web/app/configuration/configuration-setting.component.js
@@ -0,0 +1,38 @@
+﻿//configuration-setting.component.js
+(function () {
+    var module = angular.module('app');
+
+    function controller($http) {
+        var $ctrl = this;
+
+        $ctrl.$onInit = function () {
+            console.log('configuration setting init');
+            $http.get('api/configuration').then(function (r) {
+                $ctrl.configuration = r.data;
+                console.log('configuration', $ctrl.configuration);
+            });
+        }
+
+        $ctrl.cancel = function () {
+            $ctrl.dismiss();
+        }
+
+        $ctrl.save = function () {
+            console.log('save configuration', $ctrl.configuration);
+
+            return $http.post('api/configuration', $ctrl.configuration).then(function (r) {
+                angular.extend($ctrl.configuration, r.data);
+                console.log('return', r);
+            }).catch(function (err) {
+                console.error('something went wrong', err.message);
+            });
+        }
+    }
+
+    module.component('configurationSetting',
+        {
+            templateUrl: 'app/configuration/configuration-setting.component.html',
+            controller: ['$http', controller]
+        });
+}
+)();
