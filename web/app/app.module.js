f999202c257175f12eb88e7bb0e97b540e42331c
Mark Lawrence <mark.lawrence@splcenter.org>
2017-03-31 10:18:33 -0500

    added toastr messages to configuration

--- web/app/app.module.js
+++ web/app/app.module.js
@@ -1,5 +1,43 @@
 ﻿//app.module.js
-(function() {
-    var module = angular.module('app', ['app.directives', 'ui.bootstrap', 'ui.slider', 'smart-table', 'ngTagsInput']);
-    }
+(function () {
+    var module = angular.module('app', [
+        'app.directives',
+        'ui.bootstrap',
+        'ui.slider',
+        'smart-table',
+        'ngTagsInput',
+        'toastr'
+    ]);
+
+    module.config(['toastrConfig', function (toastrConfig) {
+        angular.extend(toastrConfig, {
+            positionClass: 'toast-bottom-right',
+            newestOnTop: true,
+            allowHtml: false,
+            closeButton: false,
+            closeHtml: '<button>&times;</button>',
+            extendedTimeOut: 1000,
+            iconClasses: {
+                error: 'toast-error',
+                info: 'toast-info',
+                success: 'toast-success',
+                warning: 'toast-warning'
+            },
+            messageClass: 'toast-message',
+            onHidden: null,
+            onShown: null,
+            onTap: null,
+            progressBar: false,
+            tapToDismiss: true,
+            templates: {
+                toast: 'directives/toast/toast.html',
+                progressbar: 'directives/progressbar/progressbar.html'
+            },
+            timeOut: 5000,
+            titleClass: 'toast-title',
+            toastClass: 'toast'
+        });
+    }]);
+
+}
 )();
--- web/app/configuration/configuration-setting.component.js
+++ web/app/configuration/configuration-setting.component.js
@@ -2,7 +2,7 @@
 (function () {
     var module = angular.module('app');
 
-    function controller($http) {
+    function controller($http, toastr) {
         var $ctrl = this;
         var hub = $.connection.nameNotificationHub;
 
@@ -11,6 +11,7 @@
             $.connection.hub.start().done(function(){ console.log('hub connection started'); });
             $http.get('api/configuration').then(function (r) {
                 $ctrl.configuration = r.data;
+                toastr.success('Loaded configuration');
             });
         }
 
@@ -21,8 +22,10 @@
         $ctrl.save = function () {
             return $http.post('api/configuration', $ctrl.configuration).then(function (r) {
                 angular.extend($ctrl.configuration, r.data);
+                toastr.success('Configuration Saved');
             }).catch(function (err) {
                 console.error('something went wrong', err.message);
+                toastr.error('Error Saving Configuration');
             }).finally(function () {
                 hub.server.configurationChange($ctrl.configuration).then(function (r) {
                     console.log('configuration sent to hub');
@@ -36,7 +39,7 @@
     module.component('configurationSetting',
         {
             templateUrl: 'app/configuration/configuration-setting.component.html',
-            controller: ['$http', controller]
+            controller: ['$http', 'toastr', controller]
         });
 }
 )();
